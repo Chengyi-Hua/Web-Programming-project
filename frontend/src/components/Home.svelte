412f554697ab568f12d466c8ba8e854e50deee54
Chengyi-Hua <96745479+Chengyi-Hua@users.noreply.github.com>
2022-01-22 17:37:44 +0100

    Uhrzeit
    habe es mal ausprobiert und fand es ziemlich cool

--- frontend/src/components/Home.svelte
+++ frontend/src/components/Home.svelte
@@ -2,10 +2,15 @@
   import { Request } from "https://deno.land/x/request@1.3.2/mod.ts";
   import Box from './Box.svelte';
   import SvelteTable from "./svelte-table.svelte";
+  import { time } from './Uhr.js';
 
   export let name = "JOY";
   let inputValue;
   let response;
+
+
+
+
 
   async function handleClick() {
     // const result = await Request.get("http://localhost:3004/gibmirdieloesung");
@@ -15,11 +20,14 @@
   }
 
 
+
+
+
   const rows =[
-    {RELATIONSHIPS: 'crash', MOTIVATION: 'work', RANDOM: 'facts'},
-    {RELATIONSHIPS: 'family', MOTIVATION: 'love', RANDOM: 'numbar'},
-    {RELATIONSHIPS: 'friends', MOTIVATION: 'sport', RANDOM: 'names'},
-    {RELATIONSHIPS: 'marriage', MOTIVATION: 'school', RANDOM: 'furtune cookies'},
+    {BEZIEHUNG: 'Ehe', MOTIVATION: 'Liebe', ZUFALL: 'Name'},
+    {BEZIEHUNG: 'Crash', MOTIVATION: 'Sport', ZUFALL: 'Nummer'},
+    {BEZIEHUNG: 'Freunde', MOTIVATION: 'Arbeit', ZUFALL: 'Fakten'},
+    {BEZIEHUNG: 'Familie', MOTIVATION: 'Schule', ZUFALL: 'Glückskekse'},
   ];
 
   const columns = Object.keys(rows[0]).map((key) => {
@@ -32,22 +40,24 @@
     }
   });   
 
-
+  const formatter = new Intl.DateTimeFormat('en', {
+		hour12: true,
+		hour: 'numeric',
+		minute: '2-digit',
+		second: '2-digit'
+	});
 </script>
 
 
 
 
 
-
 <div class="superStyleSuperGeil">
-  <h1> {name} </h1>
-
+  <h1> {name}  </h1>
   <Box>
   <h3>- stellt eine Frage oder hinterlasse ein Kommentar - </h3>
+  {formatter.format($time)}
   </Box>
-
-
   <p><br></p>
 
 
@@ -62,36 +72,30 @@
   {#if response !== undefined}
     {response.answer}
   {/if}
-  <br>
-  <br>
-
-
+  <br><br>
 </div>
 
 
 
 
 <div class="table">
-
-  
   <h2>
     Mögliche Themen
-  </h2>
-
-  <br>
+  </h2><br>
   <SvelteTable
     {rows}
     {columns}
   />
+  <br><br>
 </div>
 
   
-
 
 <link
   href="https://fonts.googleapis.com/css?family=Overpass:100,400"
   rel="stylesheet"
 />
+
 
 <style>
   h1 {
@@ -100,15 +104,14 @@
     font-size: 3em;
     font-weight: 100;
     margin: 1%;
-    text-align: right
-  }
+    text-align: right;
 
+  }
   h2 {
     color: #ffffff;
     text-align: right;
     margin: 1.5%;
   }
-  
   h3 {
     color: #ffffff;
     text-transform: uppercase;
@@ -122,14 +125,9 @@
   .superStyleSuperGeil {
     background-color: #688f68;
   }
-
-
   .table {
     background-color: #688f68;
     color: #ffffff;
     text-align: justify all;
   }
-
-
-
 </style>
