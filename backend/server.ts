839b13439f342dbc4d5c2651a7b49aaa8902eac9
Dennis <yassdenn@gmail.com>
2022-01-26 18:58:47 +0100

    sends appropriate response back to FE

--- backend/server.ts
+++ backend/server.ts
@@ -1,15 +1,49 @@
 import { opine } from "https://deno.land/x/opine@2.1.1/mod.ts";
 import { opineCors } from "https://deno.land/x/cors/mod.ts";
+
+const stress = 
+["relax, take it easy",
+"take a brake",
+"go on holiday"]
+
+const trauer = 
+["don't worry be happy",
+"go to your happy place",
+"try weed"]
+
+const sport = 
+["mach mehr sport",
+"Krafttraining hilf deinem Body-Image",
+"auch kleine Vorschritte sind Vortschritte"]
+
+const lernen = 
+["Setze dir dein Ziel vor augen",
+"Früh aufstehen hilft",
+"nicht alles auf den letzten Drücker"]
+
+const familie = 
+["Ruf deine Eltern an",
+"Familie sind die wahren OGs",
+"Versuche zur verstehen warum die Karen in deiner Familie so ist"]
+
+const freunde = 
+["call your friends to ckeck up on them",
+"Nur weil man noch snapt heißt das nicht das man noch in Kontakt ist",
+"love your friends"]
+
 const app = opine();
-const StressList = ["A","B","C"];
 app.use(opineCors())
 
 app.get("/problems/:typeofproblem", function (req, res) {
-    if (req.params.typeofproblem == "stress"){
-        res.send({ answer: "Die Lösung ist: Take it easy & relax & mach bissle weniger stress" })
-    } else {
-        res.send({answer: "mach was anderes"})
-    }
+    let emotion = eval(req.params.typeofproblem)
+    let index = Math.floor(Math.random()*3)
+
+    console.log(typeof(emotion))
+    console.log(emotion)
+    console.log(index)
+
+    res.send({ answer: emotion[index] })
+    
     
 });
 
